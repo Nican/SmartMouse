8f902b15da98983476f3ccf511e669afb67ae563
Nican <nican132@gmail.com>
2012-03-15 20:44:24 -0400

    Relative path for the executable.

--- MouseSimulator/src/Mouse.java
+++ MouseSimulator/src/Mouse.java
@@ -29,7 +29,7 @@ public class Mouse {
 
 		try {
 			process = runtime
-					.exec("/home/nican/workspace/SmartMouse/Debug/SmartMouse");
+					.exec("../SmartMouse/Debug/SmartMouse");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
