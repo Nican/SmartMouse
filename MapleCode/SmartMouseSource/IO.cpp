3bd7f165e425c1b79346757f58d24623fc1f4e33
Alex <ajryan@wpi.edu>
2012-04-08 19:04:48 -0400

    MoreHeadergoodiesness

---
+++ MapleCode/SmartMouseSource/IO.cpp
@@ -0,0 +1,15 @@
+//
+//  IO.cpp
+//  
+//
+//  Created by Alexander Ryan on 4/8/12.
+//  Copyright 2012 Worcester Polytechnic Institute. All rights reserved.
+//
+
+#include "IO.h"
+
+void setupIOPins(){
+    setupDriveControlPins();
+    setupSensors();
+    return;
+}
