05c31a7c77b7440ebb2064a7019725f96317d0c5
Alex <ajryan@wpi.edu>
2012-04-14 04:28:36 -0400

    Yo

--- MapleCode/SmartMouseSource/main.cpp
+++ MapleCode/SmartMouseSource/main.cpp
@@ -11,13 +11,26 @@
 #define tile_size 180
 void setupIOPins();
 int solveMaze();
-int doshit = 1;
+int doshit = 1; //lol
 int main(){
     
     setupIOPins();
     
     while(doshit){
-        SerialUSB.println("RUNNING!!!!!");   
+        SerialUSB.println("RUNNING!!!!!");
+        SerialUSB.print("Front left IR Range: ");
+        SerialUSB.println(getFrontLeftIRRange());
+        SerialUSB.print("Front right IR Range: ");
+        SerialUSB.println(getFrontRightIRRange());
+        SerialUSB.print("LeftFront IR Range: ");
+        SerialUSB.println(getLeftFrontIRRange());
+        SerialUSB.print("Right Front IR Range: ");
+        SerialUSB.println(getRightFrontIRRange());
+        SerialUSB.print("Left Sonar");
+        SerialUSB.println(getRangeUltrasonicOne());
+        SerialUSB.print("Right Sonar");
+        SerialUSB.println(getRangeUltrasonicTwo());
+        
     } //Solves maze over and over again until an error occurs
     
     return 1;
