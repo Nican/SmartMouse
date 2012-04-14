4b2315e46be4ca726a5d7e1b5b2055a1bf0c20fa
Alex <ajryan@wpi.edu>
2012-04-14 00:50:48 -0400

    Finished IR/Ultrasonic C

--- MapleCode/SmartMouseSource/Ultrasonics.cpp
+++ MapleCode/SmartMouseSource/Ultrasonics.cpp
@@ -22,8 +22,17 @@
 #define DMA_ADC_CHANNEL 1
 #define DMA_ADC_NUMBER 1
 
+#define ULTRASONIC_ADC_MODULE 2
+#define ULTRASONIC_ONE_CHANNEL 12
+#define ULTRASONIC_TWO_CHANNEL 13
+
 //Which timer to handle the automatic switching for the ultrasonics
-#define PULSING_TIMER_NUM 
+#define PULSING_TIMER_NUM 2
+#define PULSING_TIMER_CHAN 3
+
+//Sets counter to count at 1 khz, and for 80 ms interrupts
+#define ma_prescaler 72000
+#define ma_overflow 80
 
 int mostRecentUltrasonicVoltageOne = 0; //Used for holding the most recent analog reading from sensor one
 int mostRecentUltrasonicVoltageTwo = 0; //Used for holding the most recent analog reading from sensor two
@@ -32,22 +41,111 @@ int mostRecentOversampleReadingOne = 0; //Holds accumulated values for oversampl
 
 int mostRecentOversampleReadingTwo = 0; //Holds accumulated values for oversampling.
 
-char overSampleBits = 0; //Holds the number of bits to oversample
+int mostRecentUltrasonicRangeOne = 0; //mm
+
+int mostRecentUltrasonicRangeTwo = 0; //mm
+
+unsigned int lastADCTwoReadMicros = 0;
+
+unsigned int lastADCOneReadMicros = 0;
+
+char overSampleBits = 2; //Holds the number of bits to oversample
+
+char sampleUltraOneHuh 1; //
+
+HardwareTimer ultrasonicTimer(PULSING_TIMER_NUM);
+
 
 #ifdef USE_ANALOG_ENVELOPE
 
-short[ULTRASONIC_ENVELOPE_BUFFER_SIZE] ultrasonicOneBuffer;
+void sampleUltrasonics(void);
 
 short[ULTRASONIC_ENVELOPE_BUFFER_SIZE] ultrasonicOneBuffer;
 
+short[ULTRASONIC_ENVELOPE_BUFFER_SIZE] ultrasonicOneBuffer;
+
+#endif
+
+void setUltrasonicOversampleRate(int bitsOverSample){
+    overSampleBits = bitsOverSample;
+}
+
+void setupUltrasonicPins(){
+    pinMode(ULTRASONIC_ONE_VOLTAGE_PIN, ANALOG_INPUT);
+    pinMode(ULTRASONIC_TWO_VOLTAGE_PIN, ANALOG_INPUT);
+    pinmode(ULTRASONIC_ONE_ENABLE_PIN, OUTPUT);
+    pinMode(ULTRASONIC_TWO_ENABLE_PIN, OUTPUT);
+    adc_set_sample_rate(ADC2, ADC_SMPR_13_5); //sets ultrasonic ADC to sample much faster
+    digitalWrite(ULTRASONIC_ONE_VOLTAGE_PIN, LOW);
+    digitalWrite(ULTRASONIC_TWO_VOLTAGE_PIN, LOW);
+}
 
 
-void setupUltrasonicPins();
+void setupUltrasonicTimer(){
+    ultrasonicTimer.setMode(PULSING_TIMER_CHAN);
+    ultrasonicTimer.pause();
+    ultrasonicTimer.setCount(0);
+    ultrasonicTimer.setPrescaleFactor(ma_prescaler);
+    ultrasonicTimer.setOverflow(ma_overflow);
+    ultrasonicTimer.setCompare(PULSING_TIMER_CHAN, 1);
+    ultrasonicTimer.attachInterrupt(PULSING_TIMER_CHAN, sampleUltrasonics);
+    ultrasonicTimer.refresh();
+    ultrasonicTimer.resume();
+}
 
-void enableChainPulsingUltrasonics();
+void setupUltrasonics(){
+    setupUltrasonicPins();
+    setupUltrasonicTimer();
+}
 
-void enableChainPulsingUltrasonicsFreq(int delay);
+void sampleUltrasonics(void){
+    int a;
+    if(sampleUltraOneHuh){
+        lastADCOneReadMicros = micros();
+        sampleUltraOneHuh = 0;
+        for(a = 0; a < (1 << overSampleBits); a++){
+            mostRecentOversampleReadingOne += adc_read(ADC2, ULTRASONIC_ONE_CHANNEL);
+        }
+        mostRecentUltrasonicVoltageOne = mostRecentOversampleReadingOne >> overSampleBits;
+    
+        mostRecentUltrasonicRangeOne = mostRecentUltrasonicVoltageOne >> 2; //div 4
+        
+    }
+    else{
+        lastADCTwoReadMicros = micros();
+        sampleUltraOneHuh = 1;
+        for(a = 0; a < (1 << overSampleBits); a++){
+            mostRecentOversampleReadingTwo += adc_read(ADC2, ULTRASONIC_TWO_CHANNEL);
+        }
+        
+        mostRecentUltrasonicVoltageTwo = mostRecentUltrasonicVoltageTwo >> overSampleBits;
+        
+        mostRecentUltrasonicRangeTWO = mostRecentUltrasonicVoltageTwo >> 2; //div 4
+    }
+}
 
 
-void disableChainPulsingUltrasonics();
+int inline readVoltageUltrasonicOne(){
+    return mostRecentUltrasonicVoltageOne;
+}
 
+int inline readVoltageUltrasonicTwo(){
+    return mostRecentUltrasonicVoltageTwo;
+}
+
+int inline getRangeUltrasonicOne(){
+    return mostRecentUltrasonicRangeOne;
+}
+
+int inline getRangeUltrasonicTwo(){
+    return mostRecentUltrasonicRangeTwo;
+}
+
+int inline microsTimeSinceLastADCReadUltrasonicOne(){
+    return micros() - lastADCOneReadMicros;
+}
+
+
+int inline microsTimeSinceLastADCReadUltrasonicTwo(){
+    return micros() - lastADCTwoReadMicros;
+}
